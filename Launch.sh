401e6b5f554232dc961ca754ea10fb596a3b5f5e
LeonBP <leonbarbed@gmail.com>
2019-03-07 11:52:46 +0100

    Easy launch script

--- Launch.sh
+++ Launch.sh
@@ -1,6 +1,7 @@
 gnome-terminal -e 'bash -c roscore'
 sleep 2
-gnome-terminal -e 'bash -c rqt'
-gnome-terminal -e 'python Moves.py'
+gnome-terminal -e 'roslaunch rotors_gazebo mav_hovering_example.launch mav_name:=firefly world_name:=basic'
+sleep 2
 gnome-terminal -e 'python Viz_Dron.py'
-gnome-terminal -e 'python Mov_Ev.py'
+gnome-terminal -e 'python Moves.py'
+gnome-terminal -e 'bash -c "source /opt/venvs/drone-venv/bin/activate;python Mov_Ev.py"'
